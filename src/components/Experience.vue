a2ff2467ad4d81cd4ee33a4d76e90ec45c29e25f
pdev <p.developer0207@gmail.com>
2020-07-06 20:49:55 -0700

    update experience - add all history

--- src/components/Experience.vue
+++ src/components/Experience.vue
@@ -60,6 +60,53 @@ export default {
                 '- Developed architectures on top of AWS Lambda, AWS CloudFormation, and AWS S3. <br/>' +
                 '- Collaborated with other developers in an Agile environment, using Scrum methodology. <br/>',
             techs: ['Vue.js', 'Nuxt.js', 'React', 'Angular', 'AWS', 'Docker', 'Node.js', 'Serverless', 'Microservices']
+        },
+        {
+            title:'StrongBlock, Full-time',
+            logo:sb_logo, 
+            pos:'Software Engineer',
+            time:'Nov 2018 - May 2019',
+            text:'- Built and maintained development, staging, and production infrastructure on AWS <br/>' +
+                '- Built a rich SPAs using React and Vue <br/>' +
+                '- Personally supervised and trained a team of 2 front-end interns <br/>' +
+                '- Deployed the application in Docker containers using a DigitalOcean droplet <br/>',
+            techs: ['React', 'Vue', 'AWS', 'Docker', 'Linux', 'Git', 'DigitalOcean']
+        },
+        {
+            title:'Maz Systems Inc, Full-time',
+            logo:maz_logo, 
+            pos:'Software Engineer',
+            time:'Mar 2017 - Nov 2018',
+            text:'- Developed a progressive static web app with responsive UI components <br/>' +
+                '- Integrated the site with headless CMS and implemented GraphQL queries to retrieve data <br/>' +
+                '- Deployed the site in Netlify. Configured a custom domain and SSL certification <br/>' +
+                '- Bought domain at Namecheap and configured DNS records to point to Netlify name servers <br/>' +
+                '- Configured Google\'s reCaptcha and built contact form using Formik <br/>',
+            techs: ['React', 'TailwindCSS', 'Ruby on Rails', 'Google Analytics', 'GraphQL']
+        },
+        {
+            title:'Design Everest, Full-time',
+            logo:de_logo, 
+            pos:'Software Engineer',
+            time:'Jun 2016 - Feb 2017',
+            text:'- Started as lead UI/UX developer and advanced to team lead and project manager<br/>' +
+                '- Built rich SPAs using Vue/Nuxt, Angular<br/>' +
+                '- Coached other team members on process improvement, task completion, and technical challenges<br/>' +
+                '- Drove the process improvement efforts associated with the development team’s agile transformation<br/>',
+            techs: ['Vue.js', 'Nuxt.js', 'Angular', 'Linux', 'Docker', 'Git']
+        },
+        {
+            title:'Mesosphere, Full-time',
+            logo:mesosphere_logo, 
+            pos:'Software Engineer',
+            time:'Feb 2013 - Jun 2016',
+            text:'- Modernized the front-end on a legacy system to enable responsive layouts and mobile views<br/>' +
+                '- Enhanced the billing system by implementing several payment gateway integrations<br/>' +
+                '- Carried out the full E2E test infrastructure<br/>' +
+                '- Implemented multiple integrations with third parties<br/>' +
+                '- Built multiple CRUD operations, authorization, logging, and more<br/>' +
+                '- Designed and developed new data visualizations, infographics, and interfaces for an enterprise AI/IoT application using HighCharts and D3.js<br/>',
+            techs: ['AngularJs', 'Angular', 'HighCharts', 'D3.js', 'jQuery', 'Git', 'Google Map API']
         }
       ]
     }
